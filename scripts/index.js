5c9c2f11d7ebfe3a431f4fb6434b1f2cf10b328a
Oliver Richardson <123179922+Olliekse@users.noreply.github.com>
2023-03-26 21:32:15 +0300

    minor fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,37 @@
-document.querySelector(".profile__edit-btn-box").addEventListener("click", openPopup);
-document.querySelector(".popup__close").addEventListener("click", closePopup);
-
-function openPopup() {
-	document.querySelector(".popup").style.display = "initial";
-}
-
-function closePopup() {
-	document.querySelector(".popup").style.display = "none";
-}
-
 let formElement = document.querySelector(".popup");
 let nameInput = document.getElementById("name");
 let jobInput = document.getElementById("job");
 let newName = document.querySelector('.profile__name');
 let newJob = document.querySelector('.profile__job');
 
-function handleFormSubmit (evt) {
-    evt.preventDefault();
-    newName.textContent = nameInput.value;
-    newJob.textContent = jobInput.value;
-    closePopup();
+
+document.querySelector(".profile__edit-btn-box").addEventListener("click", openPopup);
+document.querySelector(".popup__close").addEventListener("click", closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+function openPopup() {
+
+	document.querySelector(".popup").classList.toggle("popup_show"); 
+	let name = newName.textContent;
+	let job = newJob.textContent;
+	nameInput.value = name;
+	jobInput.value = job;
+}
+
+function closePopup() {
+	document.querySelector(".popup").classList.toggle("popup_show");
 }
 
 
-formElement.addEventListener('submit', handleFormSubmit); 
+function handleFormSubmit (evt) {
+
+    evt.preventDefault();
+
+    newName.textContent = nameInput.value;
+    newJob.textContent = jobInput.value;
+    closePopup(); 
+}
+
+
+ 
